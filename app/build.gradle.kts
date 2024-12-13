815cda8f5071ece089329f9f5b17272e029b68a1
郑晓彬 <zxb5@meitu.com>
2024-12-13 14:51:58 +0800

    gpuImage 接入

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,4 +42,6 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    implementation("jp.co.cyberagent.android:gpuimage:2.1.0")
 }
